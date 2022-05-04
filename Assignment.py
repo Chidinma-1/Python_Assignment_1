205ea965b70b88d23f086f30cc607e494abd5934
chidinmaduru <duruchidinma@yahoo.com>
2022-05-04 21:09:24 +0100

    Adding Assignment to github

---
+++ Assignment.py
@@ -0,0 +1,32 @@
+name = input(
+    "Hi user, to calculate an average please input a maximum of three numbers press enter to continue")
+
+maximum_input_number = 3
+input_number_1 = int(input("Choose a number: "))
+input_number_2 = int(input("Choose a second number: "))
+input_number_3 = int(input("Choose a third number: "))
+
+result = ((input_number_1 + input_number_2 +
+          input_number_3)/maximum_input_number)
+
+
+print(int(result))
+
+# TASK TWO
+# input_statement = input("type your statement: ")
+
+# print(input_statement.capitalize())
+
+
+# TASK THREE
+# message = "I are learning Python"
+
+# x = message.replace("I", "You")
+# print(x)
+
+
+# TASK FOUR
+
+# text = "I hope you had fun today in class"
+
+# print(text.count("a"))
